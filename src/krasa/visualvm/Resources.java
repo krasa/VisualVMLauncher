017bcd776f60fbda8ee3c2880c2fdfd67dbb55dc
Vojtěch Krása <vojta.krasa@gmail.com>
2019-06-07 16:22:52 +0200

    #26 icons

--- src/krasa/visualvm/Resources.java
+++ src/krasa/visualvm/Resources.java
@@ -6,11 +6,11 @@ import javax.swing.*;
 
 public class Resources {
 
-	public static final Icon RUN_13 = IconLoader.getIcon("/krasa/visualvm/newRun4.png");//13
-	public static final Icon DEBUG_13 = IconLoader.getIcon("/krasa/visualvm/newDebug4.png");//13
+	public static final Icon RUN_13 = IconLoader.getIcon("/krasa/visualvm/run13.png");//13
+	public static final Icon DEBUG_13 = IconLoader.getIcon("/krasa/visualvm/debug13.png");//13
 
-	public static final Icon RUN = IconLoader.getIcon("/krasa/visualvm/newRun3.png");
-	public static final Icon DEBUG = IconLoader.getIcon("/krasa/visualvm/newDebug3.png");
-	public static final Icon CONSOLE_RUN = IconLoader.getIcon("/krasa/visualvm/console3.png");
+	public static final Icon RUN = IconLoader.getIcon("/krasa/visualvm/run16.png");
+	public static final Icon DEBUG = IconLoader.getIcon("/krasa/visualvm/debug16.png");
+	public static final Icon CONSOLE_RUN = IconLoader.getIcon("/krasa/visualvm/console16.png");
 
 }
