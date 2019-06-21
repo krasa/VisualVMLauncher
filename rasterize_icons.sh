5a522bea757974f4d218fd36491607836b24ef1d
Vojtěch Krása <vojta.krasa@gmail.com>
2019-06-21 10:07:15 +0200

    use png only for toolwindow

--- rasterize_icons.sh
+++ rasterize_icons.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
-inkscape -z -e resources/krasa/visualvm/console16@2x.png -w 32 -h 32 resources/krasa/visualvm/svg/console16.svg
-inkscape -z -e resources/krasa/visualvm/console16.png -w 16 -h 16 resources/krasa/visualvm/svg/console16.svg
+"C:\Program Files\Inkscape\inkscape.exe" -z -e resources/krasa/visualvm/debug13.png -w 13 -h 13 resources/krasa/visualvm/svg/debug16.svg          
+"C:\Program Files\Inkscape\inkscape.exe" -z -e resources/krasa/visualvm/debug13_dark.png -w 13 -h 13 resources/krasa/visualvm/svg/debug16_dark.svg
+"C:\Program Files\Inkscape\inkscape.exe" -z -e resources/krasa/visualvm/debug13@2x.png -w 26 -h 26 resources/krasa/visualvm/svg/debug16.svg
+"C:\Program Files\Inkscape\inkscape.exe" -z -e resources/krasa/visualvm/debug13@2x_dark.png -w 26 -h 26 resources/krasa/visualvm/svg/debug16_dark.svg
 
-inkscape -z -e resources/krasa/visualvm/debug16@2x.png -w 32 -h 32 resources/krasa/visualvm/svg/debug16.svg
-inkscape -z -e resources/krasa/visualvm/debug16.png -w 16 -h 16 resources/krasa/visualvm/svg/debug16.svg
-inkscape -z -e resources/krasa/visualvm/run16@2x.png -w 32 -h 32 resources/krasa/visualvm/svg/run16.svg
-inkscape -z -e resources/krasa/visualvm/run16.png -w 16 -h 16 resources/krasa/visualvm/svg/run16.svg
-
-inkscape -z -e resources/krasa/visualvm/debug13@2x.png -w 26 -h 26 resources/krasa/visualvm/svg/debug16.svg
-inkscape -z -e resources/krasa/visualvm/debug13.png -w 13 -h 13 resources/krasa/visualvm/svg/debug16.svg
-inkscape -z -e resources/krasa/visualvm/run13@2x.png -w 26 -h 26 resources/krasa/visualvm/svg/run16.svg
-inkscape -z -e resources/krasa/visualvm/run13.png -w 13 -h 13 resources/krasa/visualvm/svg/run16.svg
+"C:\Program Files\Inkscape\inkscape.exe" -z -e resources/krasa/visualvm/run13.png -w 13 -h 13 resources/krasa/visualvm/svg/run16.svg
+"C:\Program Files\Inkscape\inkscape.exe" -z -e resources/krasa/visualvm/run13_dark.png -w 13 -h 13 resources/krasa/visualvm/svg/run16_dark.svg
+"C:\Program Files\Inkscape\inkscape.exe" -z -e resources/krasa/visualvm/run13@2x.png -w 26 -h 26 resources/krasa/visualvm/svg/run16.svg
+"C:\Program Files\Inkscape\inkscape.exe" -z -e resources/krasa/visualvm/run13@2x_dark.png -w 26 -h 26 resources/krasa/visualvm/svg/run16_dark.svg
--- src/krasa/visualvm/Resources.java
+++ src/krasa/visualvm/Resources.java
@@ -9,8 +9,8 @@ public class Resources {
 	public static final Icon RUN_13 = IconLoader.getIcon("/krasa/visualvm/run13.png");//13
 	public static final Icon DEBUG_13 = IconLoader.getIcon("/krasa/visualvm/debug13.png");//13
 
-	public static final Icon RUN = IconLoader.getIcon("/krasa/visualvm/run16.png");
-	public static final Icon DEBUG = IconLoader.getIcon("/krasa/visualvm/debug16.png");
-	public static final Icon CONSOLE_RUN = IconLoader.getIcon("/krasa/visualvm/console16.png");
+	public static final Icon RUN = IconLoader.getIcon("/krasa/visualvm/svg/run16.svg");
+	public static final Icon DEBUG = IconLoader.getIcon("/krasa/visualvm/svg/debug16.svg");
+	public static final Icon CONSOLE_RUN = IconLoader.getIcon("/krasa/visualvm/svg/console16.svg");
 
 }
