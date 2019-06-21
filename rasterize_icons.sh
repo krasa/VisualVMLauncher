9c4739217dc1473b3eb7be897ac83d7440384b8f
Vojtěch Krása <vojta.krasa@gmail.com>
2019-06-21 10:08:28 +0200

    inkscape

--- rasterize_icons.sh
+++ rasterize_icons.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-"C:\Program Files\Inkscape\inkscape.exe" -z -e resources/krasa/visualvm/debug13.png -w 13 -h 13 resources/krasa/visualvm/svg/debug16.svg          
-"C:\Program Files\Inkscape\inkscape.exe" -z -e resources/krasa/visualvm/debug13_dark.png -w 13 -h 13 resources/krasa/visualvm/svg/debug16_dark.svg
-"C:\Program Files\Inkscape\inkscape.exe" -z -e resources/krasa/visualvm/debug13@2x.png -w 26 -h 26 resources/krasa/visualvm/svg/debug16.svg
-"C:\Program Files\Inkscape\inkscape.exe" -z -e resources/krasa/visualvm/debug13@2x_dark.png -w 26 -h 26 resources/krasa/visualvm/svg/debug16_dark.svg
+inkscape -z -e resources/krasa/visualvm/debug13.png -w 13 -h 13 resources/krasa/visualvm/svg/debug16.svg          
+inkscape -z -e resources/krasa/visualvm/debug13_dark.png -w 13 -h 13 resources/krasa/visualvm/svg/debug16_dark.svg
+inkscape -z -e resources/krasa/visualvm/debug13@2x.png -w 26 -h 26 resources/krasa/visualvm/svg/debug16.svg
+inkscape -z -e resources/krasa/visualvm/debug13@2x_dark.png -w 26 -h 26 resources/krasa/visualvm/svg/debug16_dark.svg
 
-"C:\Program Files\Inkscape\inkscape.exe" -z -e resources/krasa/visualvm/run13.png -w 13 -h 13 resources/krasa/visualvm/svg/run16.svg
-"C:\Program Files\Inkscape\inkscape.exe" -z -e resources/krasa/visualvm/run13_dark.png -w 13 -h 13 resources/krasa/visualvm/svg/run16_dark.svg
-"C:\Program Files\Inkscape\inkscape.exe" -z -e resources/krasa/visualvm/run13@2x.png -w 26 -h 26 resources/krasa/visualvm/svg/run16.svg
-"C:\Program Files\Inkscape\inkscape.exe" -z -e resources/krasa/visualvm/run13@2x_dark.png -w 26 -h 26 resources/krasa/visualvm/svg/run16_dark.svg
+inkscape -z -e resources/krasa/visualvm/run13.png -w 13 -h 13 resources/krasa/visualvm/svg/run16.svg
+inkscape -z -e resources/krasa/visualvm/run13_dark.png -w 13 -h 13 resources/krasa/visualvm/svg/run16_dark.svg
+inkscape -z -e resources/krasa/visualvm/run13@2x.png -w 26 -h 26 resources/krasa/visualvm/svg/run16.svg
+inkscape -z -e resources/krasa/visualvm/run13@2x_dark.png -w 26 -h 26 resources/krasa/visualvm/svg/run16_dark.svg
