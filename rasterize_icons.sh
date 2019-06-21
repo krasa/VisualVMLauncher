cdfd37d91f470fb20a23c57429b74b81df1f86fa
Vojtěch Krása <vojta.krasa@gmail.com>
2019-06-21 08:30:28 +0200

    fixed rasterize_icons.sh

--- rasterize_icons.sh
+++ rasterize_icons.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-inkscape -z -e resources/krasa/visualvm/console16@2x.png -w 26 -h 26 resources/krasa/visualvm/console16.svg
-inkscape -z -e resources/krasa/visualvm/console16.png -w 13 -h 13 resources/krasa/visualvm/console16.svg
-inkscape -z -e resources/krasa/visualvm/debug16@2x.png -w 26 -h 26 resources/krasa/visualvm/debug16.svg
-inkscape -z -e resources/krasa/visualvm/debug16.png -w 13 -h 13 resources/krasa/visualvm/debug16.svg
-inkscape -z -e resources/krasa/visualvm/run16@2x.png -w 26 -h 26 resources/krasa/visualvm/run16.svg
-inkscape -z -e resources/krasa/visualvm/run16.png -w 13 -h 13 resources/krasa/visualvm/run16.svg
+inkscape -z -e resources/krasa/visualvm/console16@2x.png -w 32 -h 32 resources/krasa/visualvm/console16.svg
+inkscape -z -e resources/krasa/visualvm/console16.png -w 16 -h 16 resources/krasa/visualvm/console16.svg
+
+inkscape -z -e resources/krasa/visualvm/debug16@2x.png -w 32 -h 32 resources/krasa/visualvm/debug16.svg
+inkscape -z -e resources/krasa/visualvm/debug16.png -w 16 -h 16 resources/krasa/visualvm/debug16.svg
+inkscape -z -e resources/krasa/visualvm/run16@2x.png -w 32 -h 32 resources/krasa/visualvm/run16.svg
+inkscape -z -e resources/krasa/visualvm/run16.png -w 16 -h 16 resources/krasa/visualvm/run16.svg
+
+inkscape -z -e resources/krasa/visualvm/debug13@2x.png -w 26 -h 26 resources/krasa/visualvm/debug13.svg
+inkscape -z -e resources/krasa/visualvm/debug13.png -w 13 -h 13 resources/krasa/visualvm/debug13.svg
+inkscape -z -e resources/krasa/visualvm/run13@2x.png -w 26 -h 26 resources/krasa/visualvm/run13.svg
+inkscape -z -e resources/krasa/visualvm/run13.png -w 13 -h 13 resources/krasa/visualvm/run13.svg
