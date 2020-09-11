06a83337b6d4a25a0b2ad2173cae9e0d44bd178b
Vojtěch Krása <vojta.krasa@gmail.com>
2020-09-11 12:44:12 +0200

    cleanup

--- src/krasa/visualvm/SourceRoots.java
+++ src/krasa/visualvm/SourceRoots.java
@@ -21,176 +21,161 @@ public class SourceRoots {
 	static String resolve(Project project, Module runConfigurationModule) {
 		//https://visualvm.github.io/sourcessupport.html
 		// --source-roots="c:\sources\root1;c:\sources\root2[subpaths=src:test\src]"
-		StringBuilder sb = new StringBuilder();
 
-		Dependencies dependencies = new Dependencies();
+		SourceRoots sourceRoots = new SourceRoots();
 
 		if (runConfigurationModule != null) {
-			dependencies.addModuleDependencies(runConfigurationModule);
+			sourceRoots.addModuleDependencies(runConfigurationModule);
 		} else {
 			ModuleManager manager = ModuleManager.getInstance(project);
 			Module[] modules = manager.getModules();
 			for (Module module : modules) {
-				dependencies.addModuleDependencies(module);
+				sourceRoots.addModuleDependencies(module);
 			}
 		}
 
-		dependencies.appendTo(sb);
-
-		String sourceRoots = removeLastSeparator(sb.toString());
-		if (SystemInfo.isWindows) {
-			sourceRoots = sourceRoots.replace("/", "\\");
-		}
-		return sourceRoots;
+		return sourceRoots.getVisualVmParameter();
 	}
 
-	private static String removeLastSeparator(String toString) {
-		if (toString.endsWith(File.pathSeparator)) {
-			return toString.substring(0, toString.length() - 1);
+
+	private MultiMap<String, String> jarsWithSubpaths = new MultiMap<>();
+	private Set<String> jars = new HashSet<>();
+	private Set<ModuleContentRoots> moduleContentRoots = new HashSet<>();
+
+	private Set<Module> cycleProtection = new HashSet<>();
+
+	private void addModuleDependencies(Module module) {
+		if (cycleProtection.contains(module)) {
+			return;
+		} else {
+			cycleProtection.add(module);
 		}
-		return toString;
-	}
 
-	private static void removeLastSeparator(StringBuilder sb, String suffix) {
-		if (sb.toString().endsWith(suffix)) {
-			sb.setLength(sb.length() - 1);
-		}
-	}
-
-	private static class Dependencies {
-
-		private MultiMap<String, String> jarsWithSubpaths = new MultiMap<>();
-		private Set<String> jars = new HashSet<>();
-		private Set<ModuleRoots> moduleRoots = new HashSet<>();
-
-		private Set<Module> cycleProtection = new HashSet<>();
-
-		private void addModuleDependencies(Module module) {
-			if (cycleProtection.contains(module)) {
-				return;
+		ModuleRootManager root = ModuleRootManager.getInstance(module);
+		OrderEntry[] orderEntries = root.getOrderEntries();
+		for (OrderEntry orderEntry : orderEntries) {
+			if (orderEntry instanceof ModuleOrderEntry) {
+				Module moduleDep = ((ModuleOrderEntry) orderEntry).getModule();
+				addModule(moduleDep);
+				addModuleDependencies(moduleDep);
+			} else if (orderEntry instanceof ModuleSourceOrderEntry) {
+				Module module1 = ((ModuleSourceOrderEntry) orderEntry).getRootModel().getModule();
+				addModule(module1);
 			} else {
-				cycleProtection.add(module);
-			}
-
-			ModuleRootManager root = ModuleRootManager.getInstance(module);
-			OrderEntry[] orderEntries = root.getOrderEntries();
-			for (OrderEntry orderEntry : orderEntries) {
-				if (orderEntry instanceof ModuleOrderEntry) {
-					Module moduleDep = ((ModuleOrderEntry) orderEntry).getModule();
-					addModule(moduleDep);
-					addModuleDependencies(moduleDep);
-				} else if (orderEntry instanceof ModuleSourceOrderEntry) {
-					Module module1 = ((ModuleSourceOrderEntry) orderEntry).getRootModel().getModule();
-					addModule(module1);
-				} else {
-					//				if (orderEntry instanceof LibraryOrderEntry || orderEntry instanceof InheritedJdkOrderEntry) {
-					//
-					//				} else {
-					//					System.err.println();
-					//				}
-					VirtualFile[] sources = orderEntry.getFiles(OrderRootType.SOURCES);
-					for (VirtualFile virtualFile : sources) {
-						add(virtualFile);
-					}
+				//				if (orderEntry instanceof LibraryOrderEntry || orderEntry instanceof InheritedJdkOrderEntry) {
+				//
+				//				} else {
+				//					System.err.println();
+				//				}
+				VirtualFile[] sources = orderEntry.getFiles(OrderRootType.SOURCES);
+				for (VirtualFile virtualFile : sources) {
+					add(virtualFile);
 				}
-
 			}
+
+		}
+	}
+
+	public void add(VirtualFile root) {
+		String path = root.getPath();
+		if (path.contains("!/")) {
+			String jar = StringUtil.substringBefore(path, "!/");
+			String subpath = StringUtil.substringAfter(path, "!/");
+			jarsWithSubpaths.putValue(jar, subpath);
+		} else {
+			jars.add(path);
+		}
+	}
+
+	public void addModule(Module module) {
+		ModuleRootManager root = ModuleRootManager.getInstance(module);
+		ContentEntry[] contentEntries = root.getContentEntries();
+		for (ContentEntry contentEntry : contentEntries) {
+			moduleContentRoots.add(new ModuleContentRoots(contentEntry));
+		}
+	}
+
+	public String getVisualVmParameter() {
+		StringBuilder sb = new StringBuilder();
+		for (ModuleContentRoots sourceRoot : moduleContentRoots) {
+			sourceRoot.appendTo(sb);
 		}
 
-		public void add(VirtualFile root) {
-			String path = root.getPath();
-			if (path.contains("!/")) {
-				String jar = StringUtil.substringBefore(path, "!/");
-				String subpath = StringUtil.substringAfter(path, "!/");
-				jarsWithSubpaths.putValue(jar, subpath);
-			} else {
-				jars.add(path);
-			}
-		}
-
-		public void addModule(Module module) {
-			ModuleRootManager root = ModuleRootManager.getInstance(module);
-			ContentEntry[] contentEntries = root.getContentEntries();
-			for (ContentEntry contentEntry : contentEntries) {
-				moduleRoots.add(new ModuleRoots(contentEntry));
-			}
-		}
-
-		public void appendTo(StringBuilder sb) {
-			for (ModuleRoots sourceRoot : moduleRoots) {
-				sourceRoot.appendTo(sb);
-			}
-
-			for (String jar : jars) {
-				sb.append(jar);
-				sb.append(File.pathSeparator);
-			}
-
-			for (Map.Entry<String, Collection<String>> stringCollectionEntry : jarsWithSubpaths.entrySet()) {
-				String key = stringCollectionEntry.getKey();
-				Collection<String> value = stringCollectionEntry.getValue();
-				appendTo(sb, key, new HashSet<>(value));
-			}
-		}
-
-
-		private void appendTo(StringBuilder sb, String key, Set<String> set) {
-			set.remove("");
-
-			if (set.isEmpty()) {
-				sb.append(key);
-			} else {
-				sb.append(key);
-
-				sb.append("[subpaths=");
-				for (String s : set) {
-					sb.append(s);
-					sb.append(":");
-				}
-				removeLastSeparator(sb, ":");
-				sb.append("]");
-			}
+		for (String jar : jars) {
+			sb.append(jar);
 			sb.append(File.pathSeparator);
 		}
 
-
-		@Override
-		public String toString() {
-			StringBuilder sb = new StringBuilder("Dependencies{" +
-				"map=\n");
-
-			sb.append("\nsourceRoot=");
-			for (ModuleRoots sourceRoot : moduleRoots) {
-				sb.append("\n\t-");
-				sourceRoot.appendTo(sb);
-			}
-
-			for (Map.Entry<String, Collection<String>> stringCollectionEntry : jarsWithSubpaths.entrySet()) {
-				sb.append("\n")
-					.append(stringCollectionEntry.getKey());
-				Collection<String> value = stringCollectionEntry.getValue();
-				HashSet<String> strings = new HashSet<>(value);
-				strings.remove("");
-				for (String s : strings) {
-					sb.append("\n\t-").append(s);
-				}
-			}
-			sb.append("\njars=");
-			for (String jar : jars) {
-				sb.append("\n\t-").append(jar);
-			}
-
-			return sb.toString();
+		for (Map.Entry<String, Collection<String>> stringCollectionEntry : jarsWithSubpaths.entrySet()) {
+			String key = stringCollectionEntry.getKey();
+			Collection<String> value = stringCollectionEntry.getValue();
+			appendTo(sb, key, new HashSet<>(value));
 		}
 
+
+		String sourceRootsText = removeLastSeparator(sb.toString());
+		if (SystemInfo.isWindows) {
+			sourceRootsText = sourceRootsText.replace("/", "\\");
+		}
+		return sourceRootsText;
 	}
 
-	private static class ModuleRoots {
+
+	private void appendTo(StringBuilder sb, String key, Set<String> subpaths) {
+		subpaths.remove("");
+
+		if (subpaths.isEmpty()) {
+			sb.append(key);
+		} else {
+			sb.append(key);
+
+			sb.append("[subpaths=");
+			for (String s : subpaths) {
+				sb.append(s);
+				sb.append(":");
+			}
+			removeLastSeparator(sb, ":");
+			sb.append("]");
+		}
+		sb.append(File.pathSeparator);
+	}
+
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder("Dependencies{" +
+			"map=\n");
+
+		sb.append("\nsourceRoot=");
+		for (ModuleContentRoots sourceRoot : moduleContentRoots) {
+			sb.append("\n\t-");
+			sourceRoot.appendTo(sb);
+		}
+
+		for (Map.Entry<String, Collection<String>> stringCollectionEntry : jarsWithSubpaths.entrySet()) {
+			sb.append("\n")
+				.append(stringCollectionEntry.getKey());
+			Collection<String> value = stringCollectionEntry.getValue();
+			HashSet<String> strings = new HashSet<>(value);
+			strings.remove("");
+			for (String s : strings) {
+				sb.append("\n\t-").append(s);
+			}
+		}
+		sb.append("\njars=");
+		for (String jar : jars) {
+			sb.append("\n\t-").append(jar);
+		}
+
+		return sb.toString();
+	}
+
+	private static class ModuleContentRoots {
 		private final ContentEntry contentEntry;
 		private final VirtualFile contentEntryFile;
 		private final List<VirtualFile> paths = new ArrayList<>();
 
-		public ModuleRoots(ContentEntry contentEntry) {
+		public ModuleContentRoots(ContentEntry contentEntry) {
 			this.contentEntry = contentEntry;
 			contentEntryFile = contentEntry.getFile();
 
@@ -216,8 +201,7 @@ public class SourceRoots {
 				sb.append(contentEntryFile.getPath());
 				sb.append("[subpaths=");
 				for (VirtualFile file : paths) {
-					String relativePath = VfsUtilCore.getRelativePath(file, contentEntryFile);
-					sb.append(relativePath);
+					sb.append(VfsUtilCore.getRelativePath(file, contentEntryFile));
 					sb.append(":");
 				}
 				removeLastSeparator(sb, ":");
@@ -231,7 +215,7 @@ public class SourceRoots {
 			if (this == o) return true;
 			if (o == null || getClass() != o.getClass()) return false;
 
-			ModuleRoots that = (ModuleRoots) o;
+			ModuleContentRoots that = (ModuleContentRoots) o;
 
 			return contentEntry != null ? contentEntry.equals(that.contentEntry) : that.contentEntry == null;
 		}
@@ -241,4 +225,18 @@ public class SourceRoots {
 			return contentEntry != null ? contentEntry.hashCode() : 0;
 		}
 	}
+
+	private static String removeLastSeparator(String toString) {
+		if (toString.endsWith(File.pathSeparator)) {
+			return toString.substring(0, toString.length() - 1);
+		}
+		return toString;
+	}
+
+	private static void removeLastSeparator(StringBuilder sb, String suffix) {
+		if (sb.substring(sb.length() - 1).equals(suffix)) {
+			sb.setLength(sb.length() - 1);
+		}
+	}
+
 }
